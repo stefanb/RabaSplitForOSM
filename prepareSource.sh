030994d8d617f902b96e6ca7024f872ab0e7ecd5
Štefan Baebler <stefan.baebler@gmail.com>
2017-06-01 05:57:32 +0200

    updated for 2017-05-31 data

--- prepareSource.sh
+++ prepareSource.sh
@@ -1,7 +1,7 @@
 #parameters:
 yyyy="2017"
 dd="31"
-mm="03"
+mm="05"
 # possible default: http://stackoverflow.com/questions/16835145/how-to-get-last-day-of-last-month-in-unix
 
 #internal variables...

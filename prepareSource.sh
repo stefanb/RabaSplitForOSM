6295062185f4357b41f2ad26a9f4a9dafae1236d
Stefan Baebler <stefan.baebler@gmail.com>
2020-02-29 20:25:00 +0100

    2020-02-29 data

--- prepareSource.sh
+++ prepareSource.sh
@@ -1,7 +1,7 @@
 #parameters:
 yyyy="2020"
-dd="31"
-mm="01"
+dd="29"
+mm="02"
 # possible default: http://stackoverflow.com/questions/16835145/how-to-get-last-day-of-last-month-in-unix
 
 #internal variables...

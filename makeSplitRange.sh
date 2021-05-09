54a6bb5f1da4a6a9d9729f603b3907c192cdecc3
Stefan Baebler <stefan.baebler@gmail.com>
2021-05-09 09:42:29 +0200

    automate date selection, put splits always in same folder

--- makeSplitRange.sh
+++ makeSplitRange.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -e
+# set -x
+
 start=$1
 end=$2
-dateCompact=${3}${4}${5}
 
-for i in $(seq $start $end); do 
+for i in $(seq $start $end); do
   echo "------ Making split ${i}: -------"
-  (time ./makeOneSplitExplode.sh $i $3 $4 $5) &> RabaSplits_${dateCompact}_EPSG4326/raba${i}log.txt;
+  (time ./makeOneSplitExplode.sh $i $3 $4 $5 $6 ) &> $6/raba${i}log.txt;
 done
--- prepareSource.sh
+++ prepareSource.sh
@@ -1,12 +1,25 @@
-#parameters:
-yyyy="2020"
-dd="30"
-mm="06"
-# possible default: http://stackoverflow.com/questions/16835145/how-to-get-last-day-of-last-month-in-unix
+#!/bin/bash
+set -e
+# set -x
+
+echo "Starting RABA split at $(date)"
+
+# default: http://stackoverflow.com/questions/16835145/how-to-get-last-day-of-last-month-in-unix
+#date --date="yesterday" +"%d-%m-%y"
+yyyy=`date -d "$(date +%Y-%m-01) -1 day" +"%Y"`
+dd=`date -d "$(date +%Y-%m-01) -1 day" +"%d"`
+mm=`date -d "$(date +%Y-%m-01) -1 day" +"%m"`
+
+#set parameters:
+#yyyy="2021"
+#dd="31"
+#mm="03"
 
 #internal variables...
 dateSource="${yyyy}_${mm}_${dd}"
 dateCompact="${yyyy}${mm}${dd}"
+
+echo "Processing RABA data for ${yyyy}-${mm}-${dd}"
 
 #------ download:------
 sourceRar="RABA_${dateSource}.RAR"
@@ -22,60 +35,23 @@ fi
 
 #----- extract: -------
 sourceFolder="RABA_${dateCompact}"
-if [ ! -d "${sourceFolder}" ]; then
-  # Control will enter here if $DIRECTORY doesn't exist
-    mkdir $sourceFolder
-    cd ${sourceFolder}
-    unrar x ../$sourceRar
-    cd ..
-else
-    echo $sourceFolder exists, using it;
-    ls -la $sourceFolder
-fi
-
-
-#etrsName="RABA_${dateCompact}_ETRS89"
-etrsName="RABA_${dateCompact}"
-if [ ! -d "${etrsName}" ]; then
-  # Control will enter here if $DIRECTORY doesn't exist
-    cd $sourceFolder
-    ../GeoCoordinateConverter/gk-shp -t 9 -dd RABA_${dateCompact}.shp ${etrsName}.shp
-    #../GeoCoordinateConverter/gk-shp -t 9 -dd RABA_20151231.shp RABA_20151231_EPSG4326.shp
-    cd ..
-    mkdir $etrsName
-    mv ${sourceFolder}/${etrsName}.* ${etrsName}
-else
-    echo $etrsName folder exists, using it;
-    ls -la $etrsName
-fi
+rm -rf ${sourceFolder}
+mkdir $sourceFolder
+cd ${sourceFolder}
+unrar x ../$sourceRar
+cd ..
 
 epsgName="RABA_${dateCompact}_EPSG4326"
-if [ ! -d "${epsgName}" ]; then
-    ogr2ogr -s_srs "EPSG:3794" -t_srs "EPSG:4326" ${epsgName} ${etrsName} -nln ${epsgName} -progress
-else
-    echo $epsgName folder exists, using it;
-    ls -la $epsgName
-fi
+rm -rf "${epsgName}"
+ogr2ogr -s_srs "EPSG:3794" -t_srs "EPSG:4326" ${epsgName} ${sourceFolder} -nln ${epsgName} -progress
+rm -rf "${sourceFolder}"
 
-targetFolder="RabaSplits_${dateCompact}_EPSG4326"
-if [ ! -d "${targetFolder}" ]; then
-  # Control will enter here if $DIRECTORY doesn't exist
-  #  mkdir RabaSplits_20151231_EPSG4326
-    mkdir $targetFolder
-    #ln -s /osm/raba/$targetFolder /osm/wwwroot/osm.si/$targetFolder
-    #ln -s /osm/raba/RabaSplitForOSM/RabaSplits_20180630_EPSG4326 /osm/raba/raba.openstreetmap.si/RabaSplits_20180630_EPSG4326
-    ln -s /osm/raba/RabaSplitForOSM/$targetFolder /osm/raba/raba.openstreetmap.si/$targetFolder
-else
-     echo Folder $targetFolder already exists, containing:
-     ls -la $targetFolder/*
-#     rm -r $targetFolder/*
-fi
+targetFolder="RabaSplits_latest_EPSG4326"
 
-nohup ./makeSplitRange.sh 1 999 ${yyyy} ${mm} ${dd} &
-nohup ./makeSplitRange.sh 1000 1999 ${yyyy} ${mm} ${dd} &
-nohup ./makeSplitRange.sh 2000 2999 ${yyyy} ${mm} ${dd} &
-nohup ./makeSplitRange.sh 3000 3640 ${yyyy} ${mm} ${dd} &
+./makeSplitRange.sh 1 3640 ${yyyy} ${mm} ${dd} ${targetFolder}
 
-echo "It is safe to ctrl+break, as it runs in background..."
-touch nohup.out
-tail -f nohup.out
+rm -rf ${epsgName}
+
+ln -s /osm/raba/RabaSplitForOSM/$targetFolder /osm/raba/raba.openstreetmap.si/RabaSplits_latest_EPSG4326
+
+echo "Finished on $(date)"
